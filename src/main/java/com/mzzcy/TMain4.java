4cee1beba0b4b93fda0a42f7c3531812c3d29c80
changyin.zhao <changyin.zhao@baifendian.com>
2019-06-28 14:40:04 +0800

    update test

--- src/main/java/com/mzzcy/TMain4.java
+++ src/main/java/com/mzzcy/TMain4.java
@@ -1,55 +1,26 @@
 package com.mzzcy;
 
-import static org.bytedeco.opencv.helper.opencv_imgcodecs.cvLoadImage;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Scanner;
-import org.bytedeco.ffmpeg.global.avcodec;
-import org.bytedeco.javacv.FFmpegFrameRecorder;
-import org.bytedeco.javacv.OpenCVFrameConverter;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author changyin.zhao on 6/12/19
  */
 public class TMain4 {
-    public static void main(String []args)
-    {
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter the directory path of images (for eg c:\\test)");
-        String imgPath=s.nextLine();
-        System.out.println("Enter the directory with video file name where resulting video will be saved (for eg c:\\test\\abc.mp4)");
-        String vidPath=s.nextLine();
-        ArrayList<String> links = new ArrayList<>();
-        File f=new File(imgPath);
-        File[] f2=f.listFiles();
-        for(File f3:f2)
-        {
-            links.add(f3.getAbsolutePath());
-        }
-        convertJPGtoMovie(links, vidPath);
-        System.out.println("Video has been created at "+vidPath);
-        s.close();
-    }
-    public static void convertJPGtoMovie(ArrayList<String> links, String vidPath)
-    {
-        OpenCVFrameConverter.ToIplImage grabberConverter = new OpenCVFrameConverter.ToIplImage();
-        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(vidPath,640,720);
-        try {
-            recorder.setFrameRate(1);
-            recorder.setVideoCodec(avcodec.AV_CODEC_ID_MPEG4);
-            recorder.setVideoBitrate(9000);
-            recorder.setFormat("mp4");
-            recorder.setVideoQuality(0); // maximum quality
-            recorder.start();
-            for (int i=0;i<links.size();i++)
-            {
-                recorder.record(grabberConverter.convert(cvLoadImage(links.get(i))));
-            }
-            recorder.stop();
-        }
-        catch (org.bytedeco.javacv.FrameRecorder.Exception e){
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+        Date d = new Date(time);
+        Timestamp t = new Timestamp(time);
+        t.setNanos(123456789);
+        System.out.println(d);
+        System.out.println(t);
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss'.'");
+        NumberFormat nf = new DecimalFormat("000000000");
+        System.out.println(df.format(t.getTime()));
+        System.out.println(nf.format(t.getNanos()));
     }
 }
