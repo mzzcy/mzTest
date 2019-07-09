3d915b81c3cc7fe5a5349924219d2828906de48c
changyin.zhao <changyin.zhao@baifendian.com>
2019-07-09 21:17:48 +0800

    二叉树demo

--- src/main/java/com/mzzcy/TMain.java
+++ src/main/java/com/mzzcy/TMain.java
@@ -1,16 +1,60 @@
 package com.mzzcy;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.apache.hadoop.fs.shell.Count;
 
 /**
  * Created by changyin.zhao on 11/2/18
  */
 public class TMain {
+    static int index = 0;
 
     public static void main(String[] args) throws IOException {
 
+        ExecutorService service = Executors.newCachedThreadPool();
+
+        Count count = new Count();
+        // 100个线程对共享变量进行加1
+        for (int i = 0; i < 300; i++) {
+            service.execute(() -> count.increase());
+        }
+
+        // 等待上述的线程执行完
+        service.shutdown();
+        try {
+            service.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(count.getCount());
+
     }
 
+    static class Count {
 
+        // 共享变量
+        private Integer count = 0;
+
+        public Integer getCount() {
+            return count;
+        }
+
+        public void increase() {
+            count++;
+        }
+    }
 
 }
--- src/main/java/com/mzzcy/TMain2.java
+++ src/main/java/com/mzzcy/TMain2.java
@@ -9,6 +9,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.imageio.ImageIO;
 
 /**
@@ -16,73 +17,68 @@ import javax.imageio.ImageIO;
  */
 public class TMain2 {
 
-  private static Dimension screenBounds;
-  public static int indexVideo = 1;
-  private static final double FRAME_RATE = 50;
+    private static Dimension screenBounds;
+    public static int indexVideo = 1;
+    private static final double FRAME_RATE = 50;
 
-  private static final int SECONDS_TO_RUN_FOR = 20;
-  private static final String OUTPUT_FILE = "/home/mzzcy/Desktop/V001.mp4";
+    private static final int SECONDS_TO_RUN_FOR = 20;
+    private static final String OUTPUT_FILE = "/home/mzzcy/Desktop/V003.mp4";
 
-  public static void main(String[] args) {
-    final IMediaWriter writer = ToolFactory.makeWriter(OUTPUT_FILE);
-    screenBounds = Toolkit.getDefaultToolkit().getScreenSize();
-    writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_H264,
-        screenBounds.width / 2, screenBounds.height / 2);
+    public static void main(String[] args) {
+        final IMediaWriter writer = ToolFactory.makeWriter(OUTPUT_FILE);
+        screenBounds = Toolkit.getDefaultToolkit().getScreenSize();
+        writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_H264, screenBounds.width / 2, screenBounds.height / 2);
 
-    long nextFrameTime = 0;
-    final long frameRate =25/1000;
-    long startTime = System.nanoTime();
+        final long frameRate = 25 / 1000;
+        long startTime = System.nanoTime();
 
-    for (int index = 1; index <= 100; index++) {
+        for (int index = 1; index <= 100; index++) {
 
-      BufferedImage bgrScreen = getVideoImage();
-      bgrScreen = convertToType(bgrScreen, BufferedImage.TYPE_3BYTE_BGR);
-      // encode the image to stream #0
-      //writer.encodeVideo(0, bgrScreen, (System.nanoTime() - startTime)/2,TimeUnit.NANOSECONDS);
-      // encode the image to stream #0
-      writer.encodeVideo(0, bgrScreen, 300*index, TimeUnit.MILLISECONDS);
-      // sleep for frame rate milliseconds
-      nextFrameTime += frameRate;
-    }
-    writer.close();
-  }
-
-  private static BufferedImage getVideoImage() {
-//    File imgLoc = new File("/home/mzzcy/Desktop/img/" + String.format("%03d", indexVideo) + ".bmp");
-
-    File imgLoc = new File("/home/mzzcy/Desktop/jpg/" + String.format("%03d", indexVideo) + ".jpg");
-    BufferedImage img = null;
-    try {
-      img = ImageIO.read(imgLoc);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    System.out.println(imgLoc.getName());
-    indexVideo++;
-    return img;
-  }
-
-  public static BufferedImage convertToType(BufferedImage sourceImage,
-      int targetType) {
-
-    BufferedImage image;
-
-    // if the source image is already the target type, return the source
-    // image
-    if (sourceImage.getType() == targetType) {
-      image = sourceImage;
-    }
-    // otherwise create a new image of the target type and draw the new
-    // image
-    else {
-      image = new BufferedImage(sourceImage.getWidth(),
-          sourceImage.getHeight(), targetType);
-      image.getGraphics().drawImage(sourceImage, 0, 0, null);
+            BufferedImage bgrScreen = getVideoImage();
+            bgrScreen = convertToType(bgrScreen, BufferedImage.TYPE_3BYTE_BGR);
+            // encode the image to stream #0
+            //writer.encodeVideo(0, bgrScreen, (System.nanoTime() - startTime)/2,TimeUnit.NANOSECONDS);
+            // encode the image to stream #0
+            writer.encodeVideo(0, bgrScreen, 200 * index, TimeUnit.MILLISECONDS);
+            // sleep for frame rate milliseconds
+        }
+        writer.close();
     }
 
-    return image;
+    private static BufferedImage getVideoImage() {
+        //    File imgLoc = new File("/home/mzzcy/Desktop/img/" + String.format("%03d", indexVideo) + ".bmp");
 
-  }
+        File imgLoc = new File("/home/mzzcy/Desktop/radar_jpg/" + String.format("0001", indexVideo) + ".jpeg");
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(imgLoc);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(imgLoc.getName());
+        indexVideo++;
+        return img;
+    }
+
+    public static BufferedImage convertToType(BufferedImage sourceImage, int targetType) {
+
+        BufferedImage image;
+
+        // if the source image is already the target type, return the source
+        // image
+        if (sourceImage.getType() == targetType) {
+            image = sourceImage;
+        }
+        // otherwise create a new image of the target type and draw the new
+        // image
+        else {
+            image = new BufferedImage(sourceImage.getWidth(), sourceImage.getHeight(), targetType);
+            image.getGraphics().drawImage(sourceImage, 0, 0, null);
+        }
+
+        return image;
+
+    }
 
 
 }
